c1a757e259f4158c34241c41044aa95338760020
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2015-11-26 01:14:01 +0100

    pre-ansible files modifications

--- pre-ansible0.sh
+++ pre-ansible0.sh
@@ -3,5 +3,5 @@
 su - -c "apt-get install sudo"
 # add user into sudo group
 su - -c "usermod -a -G sudo ${USERNAME}"
-echo "Please, logout, to apply groups changes effects"
-echo "Then run . pre-ansible1.sh"
+echo "Please, logout, to apply groups changes"
+echo "Then run ./pre-ansible1.sh"
--- pre-ansible1.sh
+++ pre-ansible1.sh
@@ -2,4 +2,3 @@
 sudo apt-get install python-pip python-dev
 sudo pip install paramiko PyYAML Jinja2 httplib2 six markupsafe
 sudo pip install ansible
-export ANSIBLE_INVENTORY="${PWD}/ansible_hosts"
