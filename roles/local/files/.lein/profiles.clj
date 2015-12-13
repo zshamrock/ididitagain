0239945bd6e6f7ceb085016bb31fe5591dec50fe
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2015-12-13 13:35:49 +0100

    Add gradle and lein dot files.

---
+++ roles/local/files/.lein/profiles.clj
@@ -0,0 +1,10 @@
+{:system 
+ {:dependencies [[org.clojure/clojure "1.7.0"]]}
+ :user {:plugins [[lein-midje "3.1.3"]
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [lein-oneoff "0.3.1"]
+                  [lein-light-nrepl "0.1.0"]
+                  [lein-kibit "0.1.2"]
+                  [venantius/ultra "0.3.4"]]
+        :ultra {:color-scheme :solarized_dark}}}
+
