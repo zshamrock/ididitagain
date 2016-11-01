b38a4ae3408f42d6b9bb23af918a0cb39db62e2f
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2016-11-01 11:40:03 +0100

    Using the project to setup LinuxMint on the new laptop

--- pre-ansible1.sh
+++ pre-ansible1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # install ansible
-sudo apt-get install python-pip python-dev
-sudo pip install paramiko PyYAML Jinja2 httplib2 six markupsafe
-sudo pip install ansible
+#sudo apt-get install python-pip python-dev
+sudo -H pip install paramiko PyYAML Jinja2 httplib2 six markupsafe
+sudo -H pip install ansible
