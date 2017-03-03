56f61c2e0111bed34508b633b0260ec7b3a8b196
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2017-03-03 22:39:26 +0100

    Update lein dependency on Clojure 1.8.0

--- roles/system/files/.lein/profiles.clj
+++ roles/system/files/.lein/profiles.clj
@@ -1,10 +1,11 @@
 {:system 
- {:dependencies [[org.clojure/clojure "1.7.0"]]}
+ {:dependencies [[org.clojure/clojure "1.8.0"]]}
  :user {:plugins [[lein-midje "3.1.3"]
                   [cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [lein-oneoff "0.3.1"]
                   [lein-light-nrepl "0.1.0"]
                   [lein-kibit "0.1.2"]
-                  [venantius/ultra "0.3.4"]]
+                  [venantius/ultra "0.3.4"]
+                  [luminus/lein-template "2.9.10.74"]]
         :ultra {:color-scheme :solarized_dark}}}
 
