8605e5c4540a93d47720dff81c161267f9389766
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2019-06-13 08:08:42 +0300

    Replace to use [[ ]] and use spaces versus tabs for formatting

--- roles/system/files/usr/local/bin/flash-mount.sh
+++ roles/system/files/usr/local/bin/flash-mount.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-if [ $# -ne 1 -o $# -ne 2 ]
+if [[ $# -ne 1 || $# -ne 2 ]]
 then
     echo "Device name is expected! Run 'dmesg' to see the device name."
     exit 1
 fi
-if [ $# -eq 1 ]; then
+if [[ $# -eq 1 ]]; then
     fstype="vfat"
     device=$1
 fi
-if [ $# -eq 2 ]; then
+if [[ $# -eq 2 ]]; then
     fstype=$1
     device=$2
 fi
 echo "Waiting to mount ${device} using ${fstype} on /media/flash..."
 for i in '' '-b' '-c' '-d'
 do
-	if [ `grep -i /media/flash$i /etc/mtab | wc -l` -eq 0 ]
-	then
-	    echo "Mounting /media/flash$i..."
+    if [[ `grep -i /media/flash$i /etc/mtab | wc -l` -eq 0 ]]
+    then
+        echo "Mounting /media/flash$i..."
         sudo mount -t ${fstype} -o utf8,gid=staff,umask=007 ${device} "/media/flash$i"
         break
-	fi	
+    fi
 done
 echo "Flash is successfully mounted )"
 
