910bf96e133fd50fda77fec1313f3ef4e8d3be52
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2019-06-13 08:01:37 +0300

    Specify the fs type to use for the mount or vfat as the default

--- roles/system/files/usr/local/bin/flash-mount.sh
+++ roles/system/files/usr/local/bin/flash-mount.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-if [ $# -ne 1 ]
+if [ $# -ne 1 -o $# -ne 2 ]
 then
     echo "Device name is expected! Run 'dmesg' to see the device name."
 	exit 1
 fi
-echo "Waiting to mount /media/flash..."
+if [ $# -eq 1 ]; then
+    fstype="vfat"
+    device=$1
+fi
+if [ $# -eq 2 ]; then
+    fstype=$1
+    device=$2
+fi
+echo "Waiting to mount ${device} using ${fstype} on /media/flash..."
 for i in '' '-b' '-c' '-d'
 do
 	if [ `grep -i /media/flash$i /etc/mtab | wc -l` -eq 0 ]
 	then
 	    echo "Mounting /media/flash$i..."
-		sudo mount -t vfat -o utf8,gid=staff,umask=007 $1 "/media/flash$i"
+		sudo mount -t ${fstype} -o utf8,gid=staff,umask=007 ${device} "/media/flash$i"
 		break
 	fi	
 done
