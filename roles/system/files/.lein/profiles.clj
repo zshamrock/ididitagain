f8e36551b831c0287232faa738d67063660513b6
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2019-07-08 21:50:43 +0300

    Sync modified files

--- roles/system/files/.lein/profiles.clj
+++ roles/system/files/.lein/profiles.clj
@@ -1,11 +1,4 @@
 {:system 
- {:dependencies [[org.clojure/clojure "1.8.0"]]}
- :user {:plugins [[lein-midje "3.1.3"]
-                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [lein-oneoff "0.3.1"]
-                  [lein-light-nrepl "0.1.0"]
-                  [lein-kibit "0.1.2"]
-                  [venantius/ultra "0.3.4"]
-                  [luminus/lein-template "2.9.10.74"]]
-        :ultra {:color-scheme :solarized_dark}}}
+ {:dependencies [[org.clojure/clojure "1.10.0"]]}
+ :user {:ultra {:color-scheme :solarized_dark}}}
 
