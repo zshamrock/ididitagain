fd55ad0183c84bf9ad16799fc149a5d30c20cf0c
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2020-07-04 22:52:10 +0300

    Structured sound system properties

---
+++ roles/sound/check-c20-hw_params.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env /bin/bash
+cat /proc/asound/C20/pcm0p/sub0/hw_params
+
+# output
+# access: MMAP_INTERLEAVED
+# format: S32_LE
+# subformat: STD
+# channels: 2
+# rate: 192000 (192000/1)
+# period_size: 65536
+# buffer_size: 131072
+
---
+++ roles/sound/check-sample-rate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env /bin/bash
+pactl list short sinks
+
+# output
+# 0       alsa_output.pci-0000_00_1f.3.analog-stereo      module-alsa-card.c      s32le 2ch 48000Hz       SUSPENDED
+# 10      alsa_output.usb-CA_CA_DacMagicXS_2.0-00.analog-stereo   module-alsa-card.c      s32le 2ch 192000Hz      RUNNING
+
